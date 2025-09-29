50c3719ebe3929ca9909d00c85e06aee2d07e6e2
rickert156 <rickert156@rambler.ru>
2025-09-29 12:52:27 +0400

    update metric

--- sinfetch.py
+++ sinfetch.py
@@ -4,7 +4,7 @@ def parseLine(line:str, pattern:str):
     kb = 'kB'
     value = line.split(pattern)[1]
     if kb in value:value = value.split(kb)[0].strip()
-    value = round(int(value) / 1024, 2)
+    value = round(int(value) / 1024, 1)
     return value
 
 def getMemInfo():
@@ -12,19 +12,28 @@ def getMemInfo():
     memAvailable = None
     memFree = None
 
+
     with open(MEMINFO_FILE, 'r') as file:
         for line in file:
             line = line.strip()
             memTotalPattern = 'MemTotal:'
             memAvailablePattern = 'MemAvailable:'
+            memInactivePattern = 'Inactive(file):'
+            memBuffersPattern = 'Buffers:'
+            memCachedPattern = 'Cached:'
 
             if memTotalPattern in line:
                 memTotal = parseLine(line=line, pattern=memTotalPattern)
             if memAvailablePattern in line:
                 memAvailable = parseLine(line=line, pattern=memAvailablePattern)
+            if memBuffersPattern in line:
+                memBuffers = parseLine(line=line, pattern=memBuffersPattern)
+            if memCachedPattern in line:
+                memCached = parseLine(line=line, pattern=memCachedPattern)
+            if memInactivePattern in line:
+                memFree = parseLine(line=line, pattern=memInactivePattern)
     
-    if memTotal != None and memAvailable != None:
-        memFree = round(memTotal - memAvailable, 2)
+    memAvailable = round(memTotal - memAvailable - memBuffers - memCached, 1) 
 
     return memTotal, memAvailable, memFree
 
